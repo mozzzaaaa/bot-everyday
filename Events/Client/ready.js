918d29c56ee15690196a176a717c0aa87d39ae7f
R0XXYY <124465481+R0XXYY@users.noreply.github.com>
2023-02-26 13:05:48 +0700

    update status

--- Events/Client/ready.js
+++ Events/Client/ready.js
@@ -1,9 +1,14 @@
-const { Client } =require('discord.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 
 module.exports = {
-    name: "ready",
-    once: true,
-    execute(client) {
-        console.log(`${client.user.username} Online`);
-    }
+  name: "ready",
+  once: true,
+  execute(client) {
+    console.log(`${client.user.username} Online`);
+
+    client.user.setPresence({
+      activities: [{ name: `EVERYDAY ROLEPLAY`, type: ActivityType.Watching }],
+      status: 'idle',
+    });
+  }
 }
